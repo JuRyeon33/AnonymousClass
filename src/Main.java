2f80642fa5ba25e985ee5c621c48f50e3d47ab54
JuRyeon33 <thwnfus0330@gmail.com>
2025-06-16 14:43:42 +0900

    AnonymousClass test

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,16 @@ interface Greeting {
     void greet();
 }
 
+
 public class Main {
     public static void main(String[] args) {
+        Greeting greeting = new Greeting() {
+            @Override
+            public void greet() {
+                System.out.println("Hello from the anonymous class!");
+            }
+        };
 
+        greeting.greet();
     }
 }
